55e31c1b16d15d056a617117837efed3657aab43
Aite <10060073+nohr@users.noreply.github.com>
2019-04-15 03:39:18 -0400

    Add files via upload

---
+++ js/fancybox.js
@@ -0,0 +1,449 @@
+(function($){
+  $(document).ready(function(){  
+      $('[data-fancybox="gallery"]').fancybox({
+	thumbs : {
+		protect: true
+	}
+});
+      $("#lab").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/nxgn/LAB.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/LAB.jpg'
+      }
+    },
+    {
+      src  : 'port/nxgn/LABback.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/LABback.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+$("#nxgn").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/nxgn/nxgn-logo.png',
+      opts : {
+        caption : '2015',
+        thumb   : 'port/nxgn/nxgn-logo.png'
+      }
+    },
+    {
+      src  : 'port/nxgn/nxgn-logo2.png',
+      opts : {
+        caption : '2016',
+        thumb   : 'port/nxgn/nxgn-logo2.png'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+  $("#realms").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/nohri/realmsofficial.jpg',
+      opts : {
+        caption : 'Front - Photographed by Jheyda Mc Garrell, Illustration by Legtorn',
+        thumb   : 'port/nohri/realmsofficial.jpg'
+      }
+    },
+    {
+      src  : 'port/nohri/realmsback.jpg',
+      opts : {
+        caption : 'Back - Illustration by Legtorn',
+        thumb   : 'port/nohri/realmsback.jpg'
+      }
+    }
+  ,
+    {
+      src  : 'port/nohri/album3.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nohri/album3.jpg'
+      }
+    },
+    {
+      src  : 'port/nohri/album4.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nohri/album4.jpg'
+      }
+    },
+    {
+      src  : 'port/nohri/realms5.jpg',
+      opts : {
+        caption : 'Photographed by Jheyda Mc Garrell, Illustration by Legtorn',
+        thumb   : 'port/nohri/realms5.jpg'
+      }
+    }], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+    $("#heatwav").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/heat-wav/heatwav.png',
+      opts : {
+        caption : 'Logo for NYC Start Up <a href="https://heatwav.co/"><u>Heat.wav</u></a>',
+        thumb   : 'port/heat-wav/heatwav.png'
+      }
+    },
+    {
+      src  : 'port/heat-wav/heatwavscreenshot.png',
+      opts : {
+        caption : 'Logo for NYC Start Up <a target="_blank" href="https://heatwav.co/"><u>Heat.wav</u></a>',
+        thumb   : 'port/heat-wav/heatwavscreenshot.png'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+     $("#eleven").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/nxgn/eleven.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/eleven.jpg'
+      }
+    },
+    {
+      src  : 'port/nxgn/rhodes.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/rhodes.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+       $("#zoom").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/nxgn/zoom.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/zoom.jpg'
+      }
+    },
+    {
+      src  : 'port/nxgn/zoomback.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/zoomback.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+       $("#mia").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/mia-gladstone/revolutionize.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/mia-gladstone/revolutionize.jpg'
+      }
+    },
+    {
+      src  : 'port/mia-gladstone/revolutionizea.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/mia-gladstone/revolutionizea.jpg'
+      }
+    },
+        {
+      src  : 'port/mia-gladstone/revolutionizeb.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/mia-gladstone/revolutionizeb.jpg'
+      }
+    },
+              {
+      src  : 'port/mia-gladstone/revolutionizec.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/mia-gladstone/revolutionizec.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+       $("#boiler").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/nxgn/boilerLibertyteeb.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/boilerLibertyteeb.jpg'
+      }
+    },
+    {
+      src  : 'port/nxgn/boilerLibertytee.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/boilerLibertytee.jpg'
+      }
+    },
+              {
+      src  : 'port/nxgn/boilerLibertywallpaper.png',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/boilerLibertywallpaper.png'
+      }
+    },
+              {
+      src  : 'port/nxgn/boilerLiberty.png',
+      opts : {
+        caption : '',
+        thumb   : 'port/nxgn/boilerLiberty.png'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+      $("#bitz").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/bitz-kids/front1.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/bitz-kids/front1.jpg'
+      }
+    },
+    {
+      src  : 'port/bitz-kids/back1.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/bitz-kids/back1.jpg'
+      }
+    },
+        {
+      src  : 'port/bitz-kids/front2.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/bitz-kids/front2.jpg'
+      }
+    },
+              {
+      src  : 'port/bitz-kids/back2.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/bitz-kids/back2.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+      $("#msc").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/nohri/msc.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nohri/msc.jpg'
+      }
+    },
+    {
+      src  : 'port/nohri/MobiusStripClub.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nohri/MobiusStripClub.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+      $("#daze").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/nohri/daze.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nohri/daze.jpg'
+      }
+    },
+    {
+      src  : 'port/nohri/valhalla.png',
+      opts : {
+        caption : '',
+        thumb   : 'port/nohri/valhalla.png'
+      }
+    },
+        {
+      src  : 'port/nohri/SZA.png',
+      opts : {
+        caption : '',
+        thumb   : 'port/nohri/SZA.png'
+      }
+    },
+              {
+      src  : 'port/nohri/rockinout.png',
+      opts : {
+        caption : '',
+        thumb   : 'port/nohri/rockinout.png'
+      }
+    },
+              {
+      src  : 'port/nohri/novinha.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/nohri/novinha.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+      $("#datb").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/4bidd/front.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/4bidd/front.jpg'
+      }
+    },
+    {
+      src  : 'port/4bidd/back.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/4bidd/back.jpg'
+      }
+    },
+              {
+      src  : 'port/4bidd/4bidd.png',
+      opts : {
+        caption : '',
+        thumb   : 'port/4bidd/4bidd.png'
+      }
+    },
+              {
+      src  : 'https://vimeo.com/89164261',
+      opts : {
+        caption : 'DIFFICULTY AT THE BEGINNING',
+        thumb   : 'port/4bidd/thumb.png'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+      $("#del").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/del-radio/delradio.png',
+      opts : {
+        caption : '',
+        thumb   : 'port/del-radio/delradio.png'
+      }
+    },
+    {
+      src  : 'port/del-radio/delradioep1.jpg',
+      opts : {
+        caption : 'Del Radio Episode 1: Party in the Skull',
+        thumb   : 'port/del-radio/delradioep1.jpg'
+      }
+    },
+              {
+      src  : 'port/del-radio/delradioep1-2.jpg',
+      opts : {
+        caption : 'Del Radio Episode 1: Party in the Skull',
+        thumb   : 'port/del-radio/delradioep1-2.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+      $("#blueprint").on('click', function() {
+          $.fancybox.open([
+    {
+      src  : 'port/jon-boy-ice/blueprint.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/jon-boy-ice/blueprint.jpg'
+      }
+    },
+    {
+      src  : 'port/jon-boy-ice/blueprinta.jpg',
+      opts : {
+        caption : '',
+        thumb   : 'port/jon-boy-ice/blueprinta.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+}); 
+  });  })(jQuery)
